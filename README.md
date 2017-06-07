68e26d652543b2c31bdee216992f4cdc8296ba90
Dipen <ddipen>
2017-06-07 19:02:03 +0530

    routing test

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # react-js
 
-## Install
+## 1. Install
 
 ```
 npm install -g create-react-app
 create-react-app react-js
+```
+
+## 2. Add Routing
+
+- Install dependencies
+
+```
+npm install react react-dom react-router@3.0.2 --save
 ```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './scene/app/App';
 import './index.css';
+// Import routing components
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Home extends React.Component {
+    render(){
+        return (<h1>Hi</h1>);
+    }
+}
+
+// More components
+class Car extends React.Component {
+    render(){
+        return (<h1>Cars page</h1>);
+    }
+}
+
+class About extends React.Component {
+    render(){
+        return (<h1>About page</h1>);
+    }
+}
+
+//render part
+
+ReactDOM.render(
+	<div>
+		<ul>
+	        <li><a href="/">Home</a></li>
+	        <li><a href="/cars">Car</a></li>
+	        <li><a href="/about">About</a></li>
+	      </ul>
+		<Router history={hashHistory}>
+	        <Route path="/" component={Home} />
+	        <Route path="/cars" component={Car} />
+	        <Route path="/about" component={About} />
+	    </Router>
+    </div>
+    , document.getElementById('root'));
