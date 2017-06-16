3fb6a3963b6456e4269811ef50c5b655b9c574a9
Dipen <ddipen>
2017-06-16 15:16:13 +0530

    folder rename

---
+++ react-js/src/scene/home/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends React.Component {
+    render(){
+        return (
+        	<div>
+        		<h1>Welcome!!</h1>
+        		<div> Go through all the tabs to learn React.</div>
+        	</div>
+        	);
+    }
+}
+
+export default Home;
