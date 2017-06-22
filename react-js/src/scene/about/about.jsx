448bc7fc36dcff3d6aea56f38a79f76be4150958
Harsh Makadia <Harsh Makadia>
2017-06-22 16:32:00 +0530

    Removed Logo

--- react-js/src/scene/about/about.jsx
+++ react-js/src/scene/about/about.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {Panel} from 'react-bootstrap';
 
 class MainPage extends React.Component {
     render(){
         return (
         	<div>
         		<h1>About</h1>
-        		<img src="http://zuru.tech/wp-content/uploads/2017/02/logo.png" />
+				<Panel>
+					<h1> Stay tunned for more updates! :P </h1>
+				</Panel>
         	</div>
         	);
     }
